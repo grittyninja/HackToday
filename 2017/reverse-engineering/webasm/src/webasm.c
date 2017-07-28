804f4587965bdfdf8cef60261cc878dcf3aa8a39
abrari <muhammad.abrari@gmail.com>
2017-07-28 17:36:34 +0700

    soal webassembly

---
+++ 2017/reverse-engineering/webasm/src/webasm.c
@@ -0,0 +1,35 @@
+// compile: 
+// 
+// emcc -o webasm.html -O2 webasm.c -s WASM=1 -s EXPORTED_FUNCTIONS="['_cekpass']" --shell-file template.html
+
+#include <stdint.h>
+
+void engkrip(uint8_t *buf, int len) {
+  int k = len;
+  int i;
+  
+  int m = 134456;
+  int c = 8121;
+  int a = 28411;
+
+  for (i = 0; i < len; i++) {
+    buf[i] ^= k;
+    k = (a*k + c) % m;
+  }
+}
+
+int cekpass(uint8_t *buf, int len) {
+  uint8_t key[29] = {118, 232, 97, 45, 109, 214, 128, 135, 32, 41, 237, 236, 26, 217, 106, 196, 199, 209, 210, 205, 155, 168, 226, 49, 120, 138, 236, 42, 74};
+  int i;
+  int c = 0;
+  
+  engkrip(buf, len);
+  
+  for (i = 0; i < len; i++) {
+    c |= buf[i] ^ key[i];
+  }  
+  
+  return (c == 0);
+}
+
+
