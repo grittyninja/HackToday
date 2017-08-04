3ae0521e6023a59ffe5cdcb493422a29d738e054
JODHI LESMANA PUTRA <jodhilesmana@gmail.com>
2017-08-05 01:08:30 +0700

    update

--- 2017-quals/web-hacking/where_it_is/src/main.py
+++ 2017-quals/web-hacking/where_it_is/src/main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__)
-
 @app.route('/')
 @app.route('/<index>')
 def root(index="index.html"):
@@ -18,7 +17,11 @@ def css():
 def send_css(filename):
     return send_from_directory('css', filename)
     
-    
+@app.route('/static/css/portal/')
+@app.route('/static/css/portal')
+def thi():
+    return 'you got the portal, now use it'
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'),404
