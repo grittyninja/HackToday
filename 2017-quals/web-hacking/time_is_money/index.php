3974a157bd6a09f01bd52b62643d2d1d1b8649a0
albertmario <albertmario19@gmail.com>
2017-08-05 18:59:16 +0700

    add time_is_money

---
+++ 2017-quals/web-hacking/time_is_money/index.php
@@ -0,0 +1,26 @@
+<html>
+<head></head>
+<body>
+	<form action="" method="POST">
+		Password : <input type="text" name="password">
+		<input type="submit" name="submit">
+	</form>
+</body>
+</html>
+
+
+
+<?php
+
+$flag ="HackToday{t111111me3e3e3e3e3e3e_15555_moneyyy_artinya_____waktu_adal777777aaaahhhh____uang__$$$$$$$$}";
+if(isset($_POST['password']))
+{
+	for($i = 0; $i < strlen($flag); $i++)
+	{
+		if($_POST['password'][$i] == $flag[$i])
+			sleep($i);
+		else
+			die("Wrong!");
+	}
+}
+?>
