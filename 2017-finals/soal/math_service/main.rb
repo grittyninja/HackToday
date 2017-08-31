998edbd23a0e71d619af53e179c9f422ad621993
pro <pro@localhost.localdomain>
2017-08-31 16:58:35 +0700

    new

--- 2017-finals/soal/math_service/main.rb
+++ 2017-finals/soal/math_service/main.rb
@@ -1,16 +1,6 @@
-puts("Selamat Datang Di Math Service")
-puts("Kamu bisa menghitung disini")
-input = gets
-
-#regex check
-# ^([-+]?[0-9]*\.?[0-9]+[\/\+\-\*])+([-+]?[0-9]*\.?[0-9]+)$
-
-valid = false
-
-
+input = $stdin.read
 if /^([-+]?[0-9]*\.?[0-9]+[\/\+\-\*])+([-+]?[0-9]*\.?[0-9]+)$/.match(input)
-
-  puts(eval(input))
+  puts eval(input)
 else
   puts("no cheating")
 end
