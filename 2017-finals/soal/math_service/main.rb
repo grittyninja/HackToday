59616159aaaf8d2d467bf5865208a8bc04a632e5
pro <pro@localhost.localdomain>
2017-09-07 06:27:18 +0700

    good delimiter

--- 2017-finals/soal/math_service/main.rb
+++ 2017-finals/soal/math_service/main.rb
@@ -1,8 +1,14 @@
-input = $stdin.read
+$/ = "="
+input = STDIN.gets()
+input = input.split(/=/).first
+STDOUT.flush
+#input = gets("\t\n")
 if /^([-+]?[0-9]*\.?[0-9]+[\/\+\-\*])+([-+]?[0-9]*\.?[0-9]+)$/.match(input)
   puts eval(input)
+  STDOUT.flush
 else
   puts("no cheating")
+  STDOUT.flush
 end
 
 
