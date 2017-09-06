cd11d9b9e63721e21a7cb8f2ff7281226de3d746
pro <pro@localhost.localdomain>
2017-09-07 06:31:18 +0700

    more hinting

--- 2017-finals/soal/math_service/main.rb
+++ 2017-finals/soal/math_service/main.rb
@@ -1,16 +1,17 @@
+puts "Hi, please input your calculation here, finished with '='"
+puts "Example: 6+5="
+STDOUT.flush
 $/ = "="
 input = STDIN.gets()
 input = input.split(/=/).first
-STDOUT.flush
 #input = gets("\t\n")
 if /^([-+]?[0-9]*\.?[0-9]+[\/\+\-\*])+([-+]?[0-9]*\.?[0-9]+)$/.match(input)
   puts eval(input)
-  STDOUT.flush
 else
   puts("no cheating")
-  STDOUT.flush
 end
 
+STDOUT.flush
 
 
 
