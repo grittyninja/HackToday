f9937a2a5540e51b268f29a305e5a6d8a4d4ce61
Fadhlal Khaliq Surado <fksutan.rajomudo@gmail.com>
2017-09-10 21:12:28 +0700

    Bersih-bersih database dan file junk

--- 2017-finals/soal/musicality_icon/content/header.php
+++ 2017-finals/soal/musicality_icon/content/header.php
@@ -1,14 +1,14 @@
  <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="/roro">Musicality</a>
+        <a class="navbar-brand" href="/">Musicality</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item <?php echo empty($_GET['p']) || $_GET['p'] === 'home' ? 'active':''; ?>">
-              <a class="nav-link" href="/roro">Home</a>
+              <a class="nav-link" href="/">Home</a>
             </li>
             <li class="nav-item <?php echo $_GET['p'] === 'popular' ? 'active':''; ?>">
               <a class="nav-link" href="#">Popular</a>
